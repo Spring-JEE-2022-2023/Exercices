c59ede2a24a225b3d49c3be3d35b6c25cf151632
Sébastien Chèvre <seb.chevre@gmail.com>
2022-08-22 22:19:51 +0200

    ui and properties

--- src/main/java/ch/hearc/beershopfull/catalog/model/Beer.java
+++ src/main/java/ch/hearc/beershopfull/catalog/model/Beer.java
@@ -7,18 +7,21 @@ package ch.hearc.beershopfull.catalog.model;
  */
 public class Beer {
 
-	public String name;
-	public Integer price;
+	private String name;
+	private Integer price;
 	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public Integer getPrice() {
 		return price;
 	}
+	
 	public void setPrice(Integer price) {
 		this.price = price;
 	}
